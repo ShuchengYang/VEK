925c8d7b3b6deff22541e6ef0da0df04e184ef53
Shucheng Yang <shucheng.yang@tum.de>
2025-08-01 15:50:24 +0200

    add ysc tag

--- vlmeval/dataset/image_mcq.py
+++ vlmeval/dataset/image_mcq.py
@@ -217,6 +217,7 @@ class ImageMCQDataset(ImageBaseDataset):
         'AI2D_TEST': '0f593e0d1c7df9a3d69bf1f947e71975',
         'AI2D_TEST_NO_MASK': 'fd8f463634d4fe9fbd23b876e8eea5be',
 #       'MMStar': 'e1ecd2140806c1b1bbf54b43372efb9e',
+        # Yang Shucheng
         'MMStar': MMSTAR_MD5,
         'RealWorldQA': '4de008f55dc4fd008ca9e15321dc44b7',
         'MLLMGuard_DS': '975fc0dd7119386e198c37d71e274b3f',
